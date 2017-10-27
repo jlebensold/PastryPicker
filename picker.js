ffaf2d0529bbe49571ab0c85ccc5e48e6a894278
Jon Lebensold <jon@lebensold.ca>
2017-10-27 11:02:27 -0400

    first commit

---
+++ picker.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Text,
+  TouchableHighlight,
+  View
+} from 'react-native';
+
+export default class Picker extends Component {
+  render() {
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+					<Text>Hi</Text>
+      </View>
+    );
+  }
+}
