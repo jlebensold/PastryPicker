d90cee185996247b21a22734a7736bd5bb76ea0d
Jon Lebensold <jon@lebensold.ca>
2017-11-15 22:41:25 -0500

    added enzyme example

--- __tests__/pastryButton.test.js
+++ __tests__/pastryButton.test.js
@@ -2,11 +2,9 @@
 import React from 'react';
 import PastryButton from '../pastryButton';
 import renderer from 'react-test-renderer';
-/*
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16'
 Enzyme.configure({adapter: new Adapter()})
-*/
 
 test('renders isActive', () => {
   const tree = renderer.create(
@@ -14,10 +12,9 @@ test('renders isActive', () => {
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });
-/*
-test('renders isActive = false', () => {
+
+test('isActive = false, background #5A8282', () => {
 	const button = shallow(
 		<PastryButton onPress={ (t) => {} } label="MyLabel" isActive={false} />);
-	console.log(button.find('TouchableHighlight'));
+  expect(button.find('TouchableHighlight').props().style[0].backgroundColor).toEqual('#5A8282');
 });
-*/
---
+++ __tests__/pastryPicker.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import PastryPicker from '../pastryPicker';
+
+test('renders correctly', () => {
+  const tree = renderer.create(
+    <PastryPicker />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
